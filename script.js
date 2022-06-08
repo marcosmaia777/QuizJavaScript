74cd686eaa825e15943fae3fdeb904cd78ca2b77
Marcos <marcosmaiafidelis777@outlook.com>
2022-06-08 15:35:26 -0300

    exibindo dados na tela

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion]
 
-        
+        document.querySelector('.scoreArea').style.display = 'none'
+        document.querySelector('.questionArea').style.display = 'block'
+
+        document.querySelector('.question').innerHTML = q.question
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div class ="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
     }
 }
