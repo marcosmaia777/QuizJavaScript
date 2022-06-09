c5684aa9e1366dba558694da1fd3cd120d04ae7f
Marcos <marcosmaiafidelis777@outlook.com>
2022-06-09 11:48:05 -0300

    reset

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let correctAnswer = 0;
 
 showQuestion();
 
+document.querySelector('.scoreArea button').addEventListener('click', reset);
+
 function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion]
@@ -23,6 +25,8 @@ function showQuestion() {
         document.querySelectorAll('.options .option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
         });
+    } else {
+        finishQuiz();
     }
 }
 
@@ -34,5 +38,32 @@ function optionClickEvent(e) {
     }
     currentQuestion++;
     showQuestion();
-    
 };
+
+function finishQuiz() {
+    let points = Math.floor((correctAnswer / questions.length) * 100);
+    if (points <= 60){
+        document.querySelector('.scoreText1').innerHTML = 'Por pouco!';
+        document.querySelector('.scorePct').style.color = '#FF0000'
+    } else if(points == 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom'
+        document.querySelector('.scorePct').style.color = 'gold'
+    } else if(points > 7) {
+        document.querySelector('.scoreText1').innerHTML = 'Excelente'
+        document.querySelector('.scorePct').style.color = '0D630D'
+    }
+    
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${correctAnswer}`
+    
+    document.querySelector('.scoreArea').style.display = 'block'
+    document.querySelector('.questionArea').style.display = 'none'
+    document.querySelector('.progress--bar').style.width = `100%`
+};
+
+function reset() {
+    correctAnswer = 0;
+    currentQuestion = 0;
+    showQuestion();
+}
